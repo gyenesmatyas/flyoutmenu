8fd432671ab9fec9b5fa989c9aa8f4f655b07ad3
gyenesmatyas <matyas.gyenes@student.kdg.be>
2021-05-12 10:03:43 +0100

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,22 @@
 
 // lange versie
 $(document).ready(function(){
-    
+    //ALS ER OP DE HAMBU
+    $("#burger").click(function(){
+        $('#nav a:not(:first-child)').toggle();
+    });
+    // WANNEER ER GERESIZED WORDT
+    $(window).resize(function(){
+        // DIT ZAL HET NORMALE MENU OPNIEUW OPENEN ALS ER GERESIZED WORDT NAAR GROOT
+        if(window.matchMedia("(min-width: 601px)").matches){
+            $('#nav a:not(:first-child)').show();
+        }
+        // DIT ZAL HET MENU SLUITEN ALS HET AAN WAS GEZET, GERESIZED WERD NAAR GROOT EN VERVOLGENS WEER GERESIZED WERD NAAR KLEIN
+        if(window.matchMedia("(max-width: 600px)").matches){
+            $('#nav a:not(:first-child)').hide();
+        }
+    })
 });
+
+
 
